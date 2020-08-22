42400b2e45becb1ac447ba5ed96034263a4e9fa9
余神 <67316978+17133851609@users.noreply.github.com>
2020-08-22 08:06:36 +0800

    8.21修订

--- scaffolding/src/views/Focus.vue
+++ scaffolding/src/views/Focus.vue
@@ -1,36 +1,67 @@
 <template>
   <div>
     <div class="fop">
+      <!-- 主题名开始 -->
       <div class="fbt">专注</div>
     </div>
+    <!-- 主题名结束 -->
     <div class="pp">
-      <a href>
+      <!-- 副主题图开始 -->
+      <router-link to="/me">
         <img
           class="fbg"
           src="../../public/resource/Pettyw160_90/pic_org_04cdce1e-3d0c-4751-8cb7-f73753f39a6b.jpg"
           alt
         />
-      </a>
+        <!-- 副主题图结束 -->
+      </router-link>
+      <!-- 小模块选项开始 -->
+      <div class="dd">
+        <router-link class="mk" to="/me">
+          <div class="mkbox">
+            <!-- 小模块选项图 -->
+            <img class="mkimg" src="../../public/resource/Mediumz140_140/109951164207707525.jpg" alt />
+            <!-- 小模块选项主题介绍 -->
+            <h3>灯下执笔</h3>
+          </div>
+        </router-link>
+        <router-link class="mk" to="/sound">
+          <div class="mkbox">
+            <img class="mkimg" src="../../public/resource/Mediumz140_140/109951164766852562.jpg" alt />
+            <h3>咖啡厅</h3>
+          </div>
+        </router-link>
+        <router-link class="mk" to="/me">
+          <div class="mkbox">
+            <img class="mkimg" src="../../public/resource/Mediumz140_140/109951164858702235.jpg" alt />
+            <h3>自习室</h3>
+          </div>
+        </router-link>
+        <router-link class="mk" to="/sound">
+          <div class="mkbox">
+            <img class="mkimg" src="../../public/resource/Mediumz140_140/109951164940992975.jpg" alt />
+            <h3>通勤路上</h3>
+          </div>
+        </router-link>
+        <router-link class="mk" to="/me">
+          <div class="mkbox">
+            <img class="mkimg" src="../../public/resource/Mediumz140_140/109951165096324064.jpg" alt />
+            <h3>图书馆</h3>
+          </div>
+        </router-link>
+         <!-- 小模块选结束 -->
+      </div>
     </div>
-    <div class="mk">
-    <a href="" > <img src="../../public/resource/Mediumz140_140/109951164207707525.jpg" alt=""><h3>灯下执笔</h3></a>
-    <a href=""> <img src="../../public/resource/Mediumz140_140/109951164766852562.jpg" alt=""><h3>咖啡厅</h3></a>
-    <a href=""> <img src="../../public/resource/Mediumz140_140/109951164858702235.jpg" alt=""><h3>自习室</h3></a>
-    <a href=""> <img src="../../public/resource/Mediumz140_140/109951164940992975.jpg" alt=""><h3>通勤路上</h3></a>
-    <a href=""> <img src="../../public/resource/Mediumz140_140/109951165096324064.jpg" alt=""><h3>图书馆</h3></a>
-    <a href=""> <img src="../../public/resource/Mediumz140_140/109951165122813288.jpg" alt=""><h3>音乐室</h3></a><a href=""> <img src="../../public/resource/Mediumz140_140/109951165150577863.jpg" alt=""><h3>深夜寝室</h3></a>
-    <a href=""> <img src="../../public/resource/Mediumz140_140/109951165153473196.jpg" alt=""><h3>考前冲刺</h3></a><a href=""> <img src="../../public/resource/Mediumz140_140/109951165156755377.jpg" alt=""><h3>灵感激发</h3></a>
-   </div>
   </div>
 </template>
 <style  scoped>
+/* 主标题字体样式 */
 .fbt {
-  /* margin-left: 1rem;
-  margin-top: 2rem; */
-  margin:3rem 1rem 1.4rem ;
+  margin: 3rem 1rem 1.4rem;
   font-size: 24px;
   font-weight: bold;
 }
+/* 主标题位置样式 */
 .fop {
   background-color: #fff;
   width: 100%;
@@ -38,30 +69,32 @@
   z-index: 10;
   margin-top: -6rem;
 }
+/* 非主标题内容规划 */
 .pp {
-  margin-top: 5rem;
-  display: flex;
-  justify-content: center;
+  margin: 5rem 1rem;
 }
+/* 副主题图样式 */
 .fbg {
-  width: 20rem;
-  /* margin:0 auto; */
+  width: 100%;
 }
-.mk{
-  margin: 2rem;
-  display: flex;
-  justify-content:space-between ;
-  flex-wrap:wrap
-
-}
-h3{
+/* 模块综合样式 */
+.mk {
+  width: 50%;
+  text-decoration: none;
   color:black;
-  margin: 0.4rem 0;
 }
-a{
-  text-decoration:none;
+/* 小模块布局方式 */
+.dd {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
 }
-img{
-  border-radius: 1rem;
+/* 小模块图片 */
+.mkimg {
+  width: 100%;
+}
+/* 小模块盒子样式 */
+.mkbox {
+  padding: 0.5rem;
 }
 </style>
